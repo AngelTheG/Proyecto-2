5b0fc607c33b7875c5761dbaa22bce8e3d75f96b
Angel_G <aguerrero20@alumnos.utalca.cl>
2022-05-31 21:48:36 -0400

    After-Hour-Udpate

--- coreWindow.py
+++ coreWindow.py
@@ -11,10 +11,11 @@ class CoreWindow(Gtk.Window):
         #Edición de la ventana principal
         super().__init__(title="Pyting Speed Test")
         self.set_border_width(50)
-        self.set_size_request(1280, 720)
+        self.set_size_request(800, 600)
         self.set_resizable(False)
 
         # Variables
+        self.readyToGo = False
         self.unstarted = True
         self.restarted = False
         self.words = 0
@@ -35,7 +36,7 @@ class CoreWindow(Gtk.Window):
         btn_restart = Gtk.Button().new_from_icon_name("process-stop", 6)
         btn_restart.connect("clicked", self.restart)
 
-        # Textviewer
+        # Textviewer texto seleccionado
         self.txt_test = Gtk.TextView()
         self.txt_test.set_editable(False)
 
@@ -45,17 +46,32 @@ class CoreWindow(Gtk.Window):
         self.ent_entry = Gtk.Entry()
         self.ent_entry.set_placeholder_text("El timer iniciará al comenzar a escribir")
         self.ent_entry.connect("changed", self.start)
+
+        # Textviewer tablas de puntaje
+        scroll = Gtk.ScrolledWindow()
+        scroll.set_hexpand(True)
+        scroll.set_vexpand(True)
+
+        self.file_scoreBoard = open("res/scoreboard","r")
+
+        self.scoreBoard = Gtk.TextView()
+        self.scoreBoard.set_editable(False)
+        self.scoreBoard.get_buffer().set_text(self.file_scoreBoard.read())
+        self.file_scoreBoard.close()
+        
+        scroll.add(self.scoreBoard)
     
         # Grid para organizar de manera más personalizada los widgets
         grid = Gtk.Grid()
-        grid.set_column_spacing(140)
+        grid.set_column_spacing(110)
         grid.set_row_spacing(30)
 
         grid.attach(btn_loadFile, 0, 0, 1, 1)
-        grid.attach_next_to(self.progressbar, btn_loadFile, Gtk.PositionType.RIGHT, 7, 1)
+        grid.attach_next_to(self.progressbar, btn_loadFile, Gtk.PositionType.RIGHT, 4, 1)
         grid.attach_next_to(btn_restart, self.progressbar, Gtk.PositionType.RIGHT, 1, 1)
-        grid.attach_next_to(self.txt_test, self.progressbar, Gtk.PositionType.BOTTOM, 7,1)
-        grid.attach_next_to(self.ent_entry, self.txt_test, Gtk.PositionType.BOTTOM, 7, 1)
+        grid.attach_next_to(self.txt_test, self.progressbar, Gtk.PositionType.BOTTOM, 4,1)
+        grid.attach_next_to(self.ent_entry, self.txt_test, Gtk.PositionType.BOTTOM, 4, 1)
+        grid.attach_next_to(scroll, self.ent_entry, Gtk.PositionType.BOTTOM,4,1)
 
         self.add(grid)
         
@@ -101,6 +117,8 @@ class CoreWindow(Gtk.Window):
 
         self.txt_test.get_buffer().set_text(self.fileText)
 
+        self.readyToGo = True
+
         dialog.destroy()
 
 
@@ -110,48 +128,52 @@ class CoreWindow(Gtk.Window):
 
 
     def start(self,widget):
-        if self.unstarted:
-            self.unstarted = False
-            self.timeout_id = GLib.timeout_add(600, self.on_timeout, None)
-        
-        # Detectar si la entrada está vacía
-        if self.ent_entry != "":
-            if " " in self.ent_entry.get_text():
-                
-                entryLetters = []
-                realLetters = []
-                for entryLetter in self.ent_entry.get_text():
-                    entryLetters.append(entryLetter)
+        if self.readyToGo:
+            if self.unstarted:
+                    self.unstarted = False
+                    self.timeout_id = GLib.timeout_add(600, self.on_timeout, None)
+            
+            # Detectar si la entrada está vacía
+            if self.ent_entry != "":
+                if " " in self.ent_entry.get_text():
+                    
+                    entryLetters = []
+                    realLetters = []
+                    for entryLetter in self.ent_entry.get_text():
+                        entryLetters.append(entryLetter)
 
-                for realLetter in self.textWords[self.wordRound]:
-                    realLetters.append(realLetter)
-                    self.letters += 1
+                    for realLetter in self.textWords[self.wordRound]:
+                        realLetters.append(realLetter)
+                        self.letters += 1
 
-                #Caso en el que la entrada es mas corta que la palabra original
-                if len(entryLetters) < len(realLetters):
-                    i = 0
-                    for letter in entryLetters:
-                        if letter == realLetters[i]:
-                            self.points += 1
-                            i += 1
+                    #Caso en el que la entrada es mas corta que la palabra original
+                    if len(entryLetters) < len(realLetters):
+                        i = 0
+                        for letter in entryLetters:
+                            if letter == realLetters[i]:
+                                self.points += 1
+                                i += 1
 
-                if len(entryLetters) > len(realLetters):
-                    i = 0
-                    for letter in realLetters:
-                        if letter == entryLetters[i]:
-                            self.points += 1
-                            i += 1
+                    if len(entryLetters) > len(realLetters):
+                        i = 0
+                        for letter in realLetters:
+                            if letter == entryLetters[i]:
+                                self.points += 1
+                                i += 1
 
-                if len(entryLetters) == len(realLetters):
-                    i = 0
-                    for letter in entryLetters:
-                        if letter == realLetters[i]:
-                            self.points += 1
-                            i += 1
+                    if len(entryLetters) == len(realLetters):
+                        i = 0
+                        for letter in entryLetters:
+                            if letter == realLetters[i]:
+                                self.points += 1
+                                i += 1
 
 
-                self.ent_entry.set_text("")
-                self.words += 1 
+                    self.ent_entry.set_text("")
+                    self.words += 1 
+
+        if self.readyToGo == False:
+            self.ent_entry.set_text("")
 
     def on_timeout(self, user_data):
         
@@ -188,6 +210,11 @@ class CoreWindow(Gtk.Window):
         Results(self)
         self.wordRound = 0
 
+    def udpateScoreBoard(self):
+        self.scoreBoard.get_buffer().set_text("")
+        self.file_scoreBoard = open("res/scoreboard","r")
+        self.scoreBoard.get_buffer().set_text(self.file_scoreBoard.read())
+        self.file_scoreBoard.close()
 
 
 #Run de la ventana principal
--- results.py
+++ results.py
@@ -8,6 +8,10 @@ from gi.repository import Gtk
 class Results(Gtk.Window):
     def __init__(self, parent):
         super().__init__(title="¡¡¡ Felicidades !!!")
+        self.set_resizable(False)
+        self.set_default()
+
+        self.core = parent
 
         #self.set_default_size(150, 100)
 
@@ -23,29 +27,35 @@ class Results(Gtk.Window):
         if parent.words <= 20:
             img_grade = img_snail
             grade = "Eres un Caracol"
+            self.resultGrade = "Caracol"
 
         if (parent.words > 20) and (parent.words <= 40):
             img_grade = img_trex
             grade = "Eres un T-Rex"
+            self.resultGrade = "T-Rex"
 
         if (parent.words > 40) and (parent.words <= 60):
             img_grade = img_monkey
             grade = "Eres un Mono"
+            self.resultGrade = "Mono"
 
         if (parent.words > 60) and (parent.words <=70):
             img_grade = img_octopus
             grade = "Eres un Pulpo"
+            self.resultGrade = "Pulpo"
 
         if parent.words > 70:
             img_grade = img_cheetah
             grade = "Eres un Leopardo"
+            self.resultGrade = "Leopardo"
 
         #Calculo de palabras por minuto
-        wpm = str(parent.words) + " palabras por minuto"
+        self.realWords = str(parent.words)
+        wpm = self.realWords + " palabras por minuto"
 
         #Calculo de precision
-        acuracy = (parent.points*100)/parent.letters
-        acuracyLBL = str(acuracy) + "% de precisión"
+        self.acuracy = str(round(((parent.points*100)/parent.letters),2)) + "%"
+        acuracyLBL = self.acuracy + " de precisión"
 
         #Calculo de proporcion
         prop = str(parent.points) + " / " + str(parent.letters)
@@ -56,6 +66,15 @@ class Results(Gtk.Window):
         lbl_precision = Gtk.Label(label = acuracyLBL)
         lbl_proportion = Gtk.Label(label = prop)
 
+        # Entry - Nombre del testeado
+        self.ent_userName = Gtk.Entry()
+        self.ent_userName.set_placeholder_text("Ingrese su nombre aquí")
+
+        # Button - Confirmar guardado de los resultados
+        btn_saveResult = Gtk.Button(label = "Guardar resultado")
+        btn_saveResult.connect("clicked", self.saveResult)
+
+
         # Contenedor de los labels de información
         infoBox = Gtk.Box()
         infoBox.set_orientation(Gtk.Orientation.VERTICAL)
@@ -65,13 +84,51 @@ class Results(Gtk.Window):
         infoBox.pack_start(lbl_precision, True, True, 0)
         infoBox.pack_start(lbl_proportion, True, True, 0)
 
-        # Contenedor Principal para organizar los widgets
-        mainBox = Gtk.Box()
-        mainBox.set_orientation(Gtk.Orientation.HORIZONTAL)
+        # Contenedor info de la tabla de resultados
+        boardBox = Gtk.Box()
+        boardBox.set_orientation(Gtk.Orientation.HORIZONTAL)
 
-        mainBox.pack_start(img_grade, True, True, 30)
-        mainBox.pack_start(infoBox, True, True, 30)
+        boardBox.pack_start(self.ent_userName, True, True, 0)
+        boardBox.pack_start(btn_saveResult, True, True, 0)
+
+        # Contenedor Principal para organizar los widgets de información
+        resultBox = Gtk.Box()
+        resultBox.set_orientation(Gtk.Orientation.HORIZONTAL)
+
+        resultBox.pack_start(img_grade, True, True, 30)
+        resultBox.pack_start(infoBox, True, True, 30)
+
+        # Contenedor principal
+        mainBox = Gtk.Box()
+        mainBox.set_orientation(Gtk.Orientation.VERTICAL)
+        
+        mainBox.pack_start(resultBox, True, True, 30)
+        mainBox.pack_start(boardBox, True, True, 30)
 
         self.add(mainBox)
 
         self.show_all()
+
+    def saveResult(self, widget):
+        if self.ent_userName.get_text() == "":
+            self.ent_userName.set_placeholder_text("Debes ingresar un nombre de usuario")
+
+        else:
+            scoreBoard = open("res/scoreboard", "a")
+
+
+            espaciado_nombre = 10-len(self.ent_userName.get_text())
+            str_username = self.ent_userName.get_text() + espaciado_nombre*" " + "|"
+            
+            espaciado_grado = 10-len(self.resultGrade)
+            str_grade = self.resultGrade + espaciado_grado*" "+"|"
+
+            espaciado_palabras = 9 - len(self.realWords)
+            str_words = self.realWords + espaciado_palabras*" "+"|"
+
+            scoreBoard.write("\n"+str_username + str_grade + str_words + self.acuracy)
+            
+            
+            scoreBoard.close()
+            self.core.udpateScoreBoard()
+            self.destroy()
